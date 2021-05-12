13be8ecc2e5de03f1c8f840298fb43975abd627a
Yilin Zhou <56918538+Liam-Zhou@users.noreply.github.com>
2021-05-11 18:02:03 -0700

    Update cmpe283-1.c

--- cmpe283-1.c
+++ cmpe283-1.c
@@ -30,7 +30,7 @@ main(int argc, char **argv)
     //     temp += (int)argv[1][i] * tens;
     //     tens = tens * 10;
     // }
-    ecx = 1;
+    ecx = 13;
     __cpuid(&eax, &ebx, &ecx, &edx);
     printf("CPUID(0x4FFFFFFE), exit number=1 exit=%u\n", eax);
 }
